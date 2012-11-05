11968098c8a5f74ffca53557d11270ba8a6226d7
Leo Lopez <llopez@makingsense.com>
2012-11-05 16:49:16 -0300

    Agregar el esqueleto del proyecto que me paso Esteban

---
+++ FuzzyRain/FuzzyRain/MainWindow.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace FuzzyRain
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private DispatcherTimer UpdateAnimationTimer;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            UpdateAnimationTimer = new DispatcherTimer();
+            UpdateAnimationTimer.Tick += new EventHandler(UpdateAnimationTimer_Tick);
+            UpdateAnimationTimer.Interval = new TimeSpan(0, 0, 0, 0, 100);
+        }
+        private void OpenFile()
+        {
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+
+            dlg.DefaultExt = ".txt";
+            dlg.Filter = "Text documents (.txt)|*.txt";
+
+            Nullable<bool> result = dlg.ShowDialog();
+
+            if (result == true)
+            {
+                string filename = dlg.FileName;
+                TextBoxArchivoEntrada.Text = filename;
+                this.ButtonComenzar.IsEnabled = true;
+            }
+        }
+
+        private void MenuItemSalir_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void MenuItemCargarLLuvia_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFile();
+        }
+
+        private void ButtonExaminar_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFile();
+        }
+
+        private void ButtonComenzar_Click(object sender, RoutedEventArgs e)
+        {
+            BeginSimulation();
+        }
+
+        private void UpdateAnimationTimer_Tick(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            double newSize = random.NextDouble() * 200;
+            ImageLLuvia.Width = newSize;
+            ImageLLuvia.Height = newSize;
+            LabelLLuvia.Content = newSize.ToString("00.00") + "mm";
+
+            double newSize1 = random.NextDouble() * 200;
+            ImageSuperficie.Width = newSize1;
+            ImageSuperficie.Height = newSize1;
+            LabelSuperficie.Content = newSize1.ToString("00.00") + "mm";
+
+            double newSize2 = random.NextDouble() * 200;
+            ImageVolumen.Width = newSize2;
+            ImageVolumen.Height = newSize2;
+            LabelVolumen.Content = newSize2.ToString("00.00") + "mm";
+        }
+
+        private void BeginSimulation()
+        {
+            UpdateAnimationTimer.Start();
+        }
+    }
+}
