ba84d50ce5fb90bbb0a4f45c31912f1a908e9150
leoslopez <llopez@makingsense.com>
2012-11-16 23:16:50 -0800

    Armado basico de logica difusa. Pruebas.

--- FuzzyRain/FuzzyRain/Model/Distribution.cs
+++ FuzzyRain/FuzzyRain/Model/Distribution.cs
@@ -5,9 +5,18 @@ using FuzzyRain.Model;
 public class Distribution
 {
     public string Name { get; set; }
-    public int RankCount { get; set; }
+    public int RankCount 
+    {
+        get
+        {
+            if (Ranks == null)
+                return 0;
+
+            return Ranks.Length;
+        }
+    }
     public Rank[] Ranks { get; set; }
-    public List<double> ValuesInOrderOfAppearance = new List<double>();
+    public IList<double> ValuesInOrderOfAppearance = new List<double>();
 
     public double Average
     {
@@ -29,16 +38,16 @@ public class Distribution
     {
         ValuesInOrderOfAppearance = new List<double>();        
     }
-    
-    public bool PutValueInRank(double value)
+
+    public bool PutValueInRank(double value, SimulationType simulationType)
     {
-        AddValueInOrderOfAppearance(value);
+        var valueToAdd = GetValueAccordingSimulationType(value, simulationType);
 
         for(int i = 0; i < RankCount; i++)
         {
-            if (value <= Ranks[i].UpperLimit)
-            {
-                Ranks[i].Values.Add(value);
+            if (valueToAdd <= Ranks[i].UpperLimit)
+            {                
+                AddValueInRank(i, valueToAdd, simulationType);                
                 return true;
             }
         }
@@ -47,9 +56,7 @@ public class Distribution
     }
 
     public double PutValueInRankUsingFrequency(double value)
-    {
-        AddValueInOrderOfAppearance(value);        
-
+    {        
         double valueToReturn = 0.0;
 
         for (int i = 0; i < RankCount; i++)
@@ -57,8 +64,12 @@ public class Distribution
             Rank currentRank = Ranks[i];
             if (value <= currentRank.CumFrequency)
             {
+                // TODO: el valor central no debiera ser tomado en cuenta, sino que debieramos obtener un valor mas preciso matematicamente 
+                //calculando proporcionalmente.
                 valueToReturn = currentRank.CenterValueOfRank;
+
                 currentRank.Values.Add(valueToReturn);
+                AddValueInOrderOfAppearance(valueToReturn);
                 return valueToReturn;
             }
         }
@@ -73,7 +84,7 @@ public class Distribution
         // manera adecuada.
         // Similar sería si se pretende una simulación semanal, deberia dividirse el dato ingresado por 4 e ingresarlo como que ocurrió en 4 oportunidades.
 
-        var valueToAdd = value / (int)simulationType;
+        var valueToAdd = GetValueAccordingSimulationType(value, simulationType);
 
         for (int i = 1; i <= (int)simulationType; i++)
         {
@@ -84,5 +95,51 @@ public class Distribution
     public void AddValueInOrderOfAppearance(double value)
     {
         ValuesInOrderOfAppearance.Add(value);
+    }
+
+    private double GetValueAccordingSimulationType(double value, SimulationType simulationType)
+    { 
+        return value / (int)simulationType;
+    }
+
+    private void AddValueAccordingSimulationType(IList<double> list, double value, SimulationType simulationType)
+    {
+        for (int i = 1; i <= (int)simulationType; i++)
+        {
+            list.Add(value);
+        }
+    }
+
+    private void AddValueInRank(int indexRank, double value, SimulationType simulationType)
+    {
+        for (int i = 1; i <= (int)simulationType; i++)
+        {
+            Ranks[indexRank].Values.Add(value);
+            AddValueInOrderOfAppearance(value);
+        }
+
+        double allValuesCount = (double)ValuesInOrderOfAppearance.Count;
+
+        for (int k = 0; k < RankCount; k++)
+        {
+            if (k != 0)
+            {
+                Ranks[k].CumFrequency = Ranks[k - 1].CumFrequency + (double)Ranks[k].Values.Count / allValuesCount;
+            }
+            else
+            {
+                Ranks[k].CumFrequency = (double)Ranks[k].Values.Count / allValuesCount;                
+            }
+        }
+
+        //double cumulative = 0;
+        //for (int j = 0; j < indexRank; j++)
+        //{
+        //    cumulative = cumulative + Ranks[j].CumFrequency;
+        //}
+
+        //double frequencyInThisRank = (double)Ranks[indexRank].Values.Count / (double)ValuesInOrderOfAppearance.Count;
+
+        //Ranks[indexRank].CumFrequency = cumulative + frequencyInThisRank;
     }
 }
